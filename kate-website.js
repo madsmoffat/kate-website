c9a938308b9e87efdaaf22e2673dca827ea82bd4
modmakermads <maddiemoffat95@gmal.com>
2021-02-19 16:15:30 -0700

    java setup

---
+++ kate-website.js
@@ -0,0 +1,288 @@
+var pages =[
+    0 ={
+        text: "",
+        illustration: ""
+    },
+    1={
+        text: "",
+        illustration: ""
+    },
+    2={
+        text: "",
+        illustration: ""
+    },
+    3={
+        text: "",
+        illustration: ""
+    },
+    4={
+        text: "",
+        illustration: ""
+    },
+    5={
+        text: "",
+        illustration: ""
+    },
+    6={
+        text: "",
+        illustration: ""
+    },
+    7={
+        text: "",
+        illustration: ""
+    },
+    8={
+        text: "",
+        illustration: ""
+    },
+    9={
+        text: "",
+        illustration: ""
+    },
+    10={
+        text: "",
+        illustration: ""
+    },
+    11={
+        text: "",
+        illustration: ""
+    },
+    12={
+        text: "",
+        illustration: ""
+    },
+    13={
+        text: "",
+        illustration: ""
+    },
+    14={
+        text: "",
+        illustration: ""
+    },
+    15={
+        text: "",
+        illustration: ""
+    },
+    16={
+        text: "",
+        illustration: ""
+    },
+    17={
+        text: "",
+        illustration: ""
+    },
+    18={
+        text: "",
+        illustration: ""
+    },
+    19={
+        text: "",
+        illustration: ""
+    },
+    20={
+        text: "",
+        illustration: ""
+    },
+    21={
+        text: "",
+        illustration: ""
+    },
+    22={
+        text: "",
+        illustration: ""
+    },
+    23={
+        text: "",
+        illustration: ""
+    },
+    24={
+        text: "",
+        illustration: ""
+    },
+    25={
+        text: "",
+        illustration: ""
+    },
+    26={
+        text: "",
+        illustration: ""
+    },
+    27={
+        text: "",
+        illustration: ""
+    },
+    28={
+        text: "",
+        illustration: ""
+    },
+    29={
+        text: "",
+        illustration: ""
+    },
+    30={
+        text: "",
+        illustration: ""
+    },
+    31={
+        text: "",
+        illustration: ""
+    },
+    32={
+        text: "",
+        illustration: ""
+    },
+    33={
+        text: "",
+        illustration: ""
+    },
+    34={
+        text: "",
+        illustration: ""
+    },
+    35={
+        text: "",
+        illustration: ""
+    },
+    36={
+        text: "",
+        illustration: ""
+    },
+    37={
+        text: "",
+        illustration: ""
+    },
+    38={
+        text: "",
+        illustration: ""
+    },
+    39={
+        text: "",
+        illustration: ""
+    },
+    40={
+        text: "",
+        illustration: ""
+    },
+    41={
+        text: "",
+        illustration: ""
+    },
+    42={
+        text: "",
+        illustration: ""
+    },
+    43={
+        text: "",
+        illustration: ""
+    },
+    44={
+        text: "",
+        illustration: ""
+    },
+    45={
+        text: "",
+        illustration: ""
+    },
+    46={
+        text: "",
+        illustration: ""
+    },
+    47={
+        text: "",
+        illustration: ""
+    },
+    48={
+        text: "",
+        illustration: ""
+    },
+    49={
+        text: "",
+        illustration: ""
+    },
+    50={
+        text: "",
+        illustration: ""
+    },
+    51={
+        text: "",
+        illustration: ""
+    },
+    52={
+        text: "",
+        illustration: ""
+    },
+    53={
+        text: "",
+        illustration: ""
+    },
+    54={
+        text: "",
+        illustration: ""
+    },
+    55={
+        text: "",
+        illustration: ""
+    },
+    56={
+        text: "",
+        illustration: ""
+    },
+    57={
+        text: "",
+        illustration: ""
+    },
+    58={
+        text: "",
+        illustration: ""
+    },
+    59={
+        text: "",
+        illustration: ""
+    },
+    60={
+        text: "",
+        illustration: ""
+    },
+    61={
+        text: "",
+        illustration: ""
+    },
+    62={
+        text: "",
+        illustration: ""
+    },
+    63={
+        text: "",
+        illustration: ""
+    },
+    64={
+        text: "",
+        illustration: ""
+    },
+    65={
+        text: "",
+        illustration: ""
+    },
+    66={
+        text: "",
+        illustration: ""
+    },
+    67={
+        text: "",
+        illustration: ""
+    },
+    68={
+        text: "",
+        illustration: ""
+    },
+    69={
+        text: "",
+        illustration: ""
+    },
+    70={
+        text: "",
+        illustration: ""
+    },
+    
+];
+
