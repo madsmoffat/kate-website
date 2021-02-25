8a728c5d353ad992899f57ed71be1a9aeb633f2d
modmakermads <maddiemoffat95@gmal.com>
2021-02-25 13:19:59 -0700

    adding photos

--- kate-website.js
+++ kate-website.js
@@ -1,288 +1,296 @@
+const img = document.createElement('img')
+
 var pages =[
     0 ={
         text: "",
-        illustration: ""
+        illustration: img.src='/0000cover.png'
     },
     1={
         text: "",
-        illustration: ""
+        illustration: img.src='/1.jpeg'
     },
     2={
         text: "",
-        illustration: ""
+        illustration: img.src='/2.jpeg'
     },
     3={
         text: "",
-        illustration: ""
+        illustration: img.src='/3.jpeg'
     },
     4={
         text: "",
-        illustration: ""
+        illustration: img.src='/4.jpeg'
     },
     5={
         text: "",
-        illustration: ""
+        illustration: img.src='/5.jpeg'
     },
     6={
         text: "",
-        illustration: ""
+        illustration: img.src='/6.jpeg'
     },
     7={
         text: "",
-        illustration: ""
+        illustration: img.src='/7.jpeg'
     },
     8={
         text: "",
-        illustration: ""
+        illustration: img.src='8.jpeg'
     },
     9={
         text: "",
-        illustration: ""
+        illustration: img.src='9.jpeg'
     },
     10={
         text: "",
-        illustration: ""
+        illustration: img.src='10.jpeg'
     },
     11={
         text: "",
-        illustration: ""
+        illustration: img.src='/11.jpeg'
     },
     12={
         text: "",
-        illustration: ""
+        illustration: img.src='/12.jpeg'
     },
     13={
         text: "",
-        illustration: ""
+        illustration: img.src='/13.jpeg'
     },
     14={
         text: "",
-        illustration: ""
+        illustration: img.src='/14.jpeg'
     },
     15={
         text: "",
-        illustration: ""
+        illustration: img.src='/15.jpeg'
     },
     16={
         text: "",
-        illustration: ""
+        illustration: img.src='/16.jpeg'
     },
     17={
         text: "",
-        illustration: ""
+        illustration: img.src='/17.jpeg'
     },
     18={
         text: "",
-        illustration: ""
+        illustration: img.src='/18.jpeg'
     },
     19={
         text: "",
-        illustration: ""
+        illustration: img.src='/19.jpeg'
     },
     20={
         text: "",
-        illustration: ""
+        illustration: img.src='/20.jpeg'
     },
     21={
         text: "",
-        illustration: ""
+        illustration: img.src='/21.jpeg'
     },
     22={
         text: "",
-        illustration: ""
+        illustration: img.src='/22.jpeg'
     },
     23={
         text: "",
-        illustration: ""
+        illustration: img.src='/23.jpeg'
     },
     24={
         text: "",
-        illustration: ""
+        illustration: img.src='/24.jpeg'
     },
     25={
         text: "",
-        illustration: ""
+        illustration: img.src='/25.jpeg'
     },
     26={
         text: "",
-        illustration: ""
+        illustration: img.src='/26.jpeg'
     },
     27={
         text: "",
-        illustration: ""
+        illustration: img.src='/27.jpeg'
     },
     28={
         text: "",
-        illustration: ""
+        illustration: img.src='/28.jpeg'
     },
     29={
         text: "",
-        illustration: ""
+        illustration: img.src='/29.jpeg'
     },
     30={
         text: "",
-        illustration: ""
+        illustration: img.src='/30.jpeg'
     },
     31={
         text: "",
-        illustration: ""
+        illustration: img.src='/31.jpeg'
     },
     32={
         text: "",
-        illustration: ""
+        illustration: img.src='/32.jpeg'
     },
     33={
         text: "",
-        illustration: ""
+        illustration: img.src='/33.jpeg'
     },
     34={
         text: "",
-        illustration: ""
+        illustration: img.src='/34.jpeg'
     },
     35={
         text: "",
-        illustration: ""
+        illustration: img.src='/35.jpeg'
     },
     36={
         text: "",
-        illustration: ""
+        illustration: img.src='/36.jpeg'
     },
     37={
         text: "",
-        illustration: ""
+        illustration: img.src='/37.jpeg'
     },
     38={
         text: "",
-        illustration: ""
+        illustration: img.src='/38.jpeg'
     },
     39={
         text: "",
-        illustration: ""
+        illustration: img.src='/39.jpeg'
     },
     40={
         text: "",
-        illustration: ""
+        illustration: img.src='/40.jpeg'
     },
     41={
         text: "",
-        illustration: ""
+        illustration: img.src='/41.jpeg'
     },
     42={
         text: "",
-        illustration: ""
+        illustration: img.src='/42.jpeg'
     },
     43={
         text: "",
-        illustration: ""
+        illustration: img.src='/43.jpeg'
     },
     44={
         text: "",
-        illustration: ""
+        illustration: img.src='/44.jpeg'
     },
     45={
         text: "",
-        illustration: ""
+        illustration: img.src='/45.jpeg'
     },
     46={
         text: "",
-        illustration: ""
+        illustration: img.src='/46.jpeg'
     },
     47={
         text: "",
-        illustration: ""
+        illustration: img.src='/47.jpeg'
     },
     48={
         text: "",
-        illustration: ""
+        illustration: img.src='/48.jpeg'
     },
     49={
         text: "",
-        illustration: ""
+        illustration: img.src='/49.jpeg'
     },
     50={
         text: "",
-        illustration: ""
+        illustration: img.src='/50.jpeg'
     },
     51={
         text: "",
-        illustration: ""
+        illustration: img.src='/51.jpeg'
     },
     52={
         text: "",
-        illustration: ""
+        illustration: img.src='/52.jpeg'
     },
     53={
         text: "",
-        illustration: ""
+        illustration: img.src='/53.jpeg'
     },
     54={
         text: "",
-        illustration: ""
+        illustration: img.src='/54.jpeg'
     },
     55={
         text: "",
-        illustration: ""
+        illustration: img.src='/55.jpeg'
     },
     56={
         text: "",
-        illustration: ""
+        illustration: img.src='/56.jpeg'
     },
     57={
         text: "",
-        illustration: ""
+        illustration: img.src='/57.jpeg'
     },
     58={
         text: "",
-        illustration: ""
+        illustration: img.src='/58.jpeg'
     },
     59={
         text: "",
-        illustration: ""
+        illustration: img.src='/59.jpeg'
     },
     60={
         text: "",
-        illustration: ""
+        illustration: img.src='/60.jpeg'
     },
     61={
         text: "",
-        illustration: ""
+        illustration: img.src='/61.jpeg'
     },
     62={
         text: "",
-        illustration: ""
+        illustration: img.src='/62.jpeg'
     },
     63={
         text: "",
-        illustration: ""
+        illustration: img.src='/63.jpeg'
     },
     64={
         text: "",
-        illustration: ""
+        illustration: img.src='/64.jpeg'
     },
     65={
         text: "",
-        illustration: ""
+        illustration: img.src='/65.jpeg'
     },
     66={
         text: "",
-        illustration: ""
+        illustration: img.src='/66.jpeg'
     },
     67={
         text: "",
-        illustration: ""
+        illustration: img.src='/67.jpeg'
     },
     68={
         text: "",
-        illustration: ""
+        illustration: img.src='/68.jpeg'
     },
     69={
         text: "",
-        illustration: ""
+        illustration: img.src='/69.jpeg'
     },
     70={
         text: "",
-        illustration: ""
+        illustration: img.src='/70.jpeg'
     },
-    
+    71 ={
+        text:"",
+        illustration: img.src='/59.jpeg'    },
+    72={
+        text:"",
+        illustration: img.src='/55.jpeg'
+    }
 ];
 
