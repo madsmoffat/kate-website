496717e93d98e5c23072abe4fdcde883ac101193
modmakermads <maddiemoffat95@gmal.com>
2021-03-11 17:47:21 -0700

    adding js-button elements to main and page array

--- kate-website.js
+++ kate-website.js
@@ -1,13 +1,590 @@
+var img = document.createElement("img"); 
+ 
+var pages =[
+    1=={
+        text: "page 1",
+        illustration: img.src='/1.jpeg',
+        links: {
+            2: "2",
+            10: "10"
+        }
+    },
+    2=={
+        text: "page 2",
+        illustration: img.src='/2.jpeg',
+        links: {
+            6: "6",
+            3: "3"
+        }
+    },
+    3=={
+        text: "page 3",
+        illustration: img.src='/3.jpeg',
+        links: {
+            4: "4",
+            5: "5"
+        }
+    },
+    4=={
+        text: "page 4",
+        illustration: img.src='/4.jpeg',
+        links: {
+            9: "9",
+            24: "24"
+        }
+    },
+    5=={
+        text: "page 5",
+        illustration: img.src='/5.jpeg', 
+        links: {
+            21: "21",
+            24: "24"
+        }
+    },
+    6=={
+        text: "page 6",
+        illustration: img.src='/6.jpeg',
+        links: {
+            8:"8",
+            21:"21"
+        }
+    },
+    7=={
+        text: "page 7",
+        illustration: img.src='/7.jpeg',
+        links: {
+            70: "70",
+            71:"71"
+        }
+    },
+    8=={
+        text: "page 8",
+        illustration: img.src='8.jpeg',
+        links: {
+            12:"12",
+            11:"11"
+        }
+    },
+    9=={
+        text: "page 9",
+        illustration: img.src='9.jpeg',
+        links: {
+            5:"5",
+            20:"20"
+        }
+    },
+    10=={
+        text: "page 10",
+        illustration: img.src='10.jpeg',
+        links:{
+            11: "11",
+            13: "13"
+        }
+    },
+    11=={
+        text: "page 11",
+        illustration: img.src='/11.jpeg',
+        links: {
+            12:"12",
+            14:"14"
+        }
+    },
+    12=={
+        text: "page 12",
+        illustration: img.src='/12.jpeg',
+        links: {
+            17: "17",
+            29: "29"
+        }
+    },
+    13=={
+        text: "page 13",
+        illustration: img.src='/13.jpeg',
+        links: {
+            14:"14",
+            15:"15"
+        }
+    },
+    14=={
+        text: "page 14",
+        illustration: img.src='/14.jpeg',
+        links: {
+            18:"18",
+            19:"19"
+        }
+    },
+    15=={
+        text: "page 15",
+        illustration: img.src='/15.jpeg',
+        links: {
+            16:"16",
+            71:"71"
+        }
+    },
+    16=={
+        text: "page 16",
+        illustration: img.src='/16.jpeg',
+        links: {
+         33:"33",
+         32:"32"
+        }
+    },
+    17=={
+        text: "page 17",
+        illustration: img.src='/17.jpeg',
+        links: {
+         27:"27",
+         28:"28" 
+        }
+    },
+    18=={
+        text: "page 18",
+        illustration: img.src='/18.jpeg',
+        links: {
+         30:"30",
+         31:"31"
+        }
+    },
+    19=={
+        text: "page 19",
+        illustration: img.src='/19.jpeg',
+        links: {
+         31:"31",
+         32:"32" 
+        }
+    },
+    20=={
+        text: "page 20",
+        illustration: img.src='/20.jpeg',
+        links: {
+         35:"35",
+         65: "65"
+        }
+    },
+    21=={
+        text: "page 21",
+        illustration: img.src='/21.jpeg',
+        links: {
+            22:"22",
+            23:"23"
+        }
+    },
+    22=={
+        text: "page 22",
+        illustration: img.src='/22.jpeg',
+        links: {
+         7:"7",
+         70:"70" 
+        }
+    },
+    23=={
+        text: "page 23",
+        illustration: img.src='/23.jpeg',
+        links: {
+         8:"8",
+         20:"20"  
+        }
+    },
+    24=={
+        text: "page 24",
+        illustration: img.src='/24.jpeg',
+        links: {
+          25:"25",
+          22:"22"
+        }
+    },
+    25=={
+        text: "page 25",
+        illustration: img.src='/25.jpeg',
+        links: {
+         22:"22",
+         26:"26"  
+        }
+    },
+    26=={
+        text: "page 26",
+        illustration: img.src='/26.jpeg',
+        links: {
+         20:"20",
+         8:"8"
+        }
+    },
+    27=={
+        text: "page 27",
+        illustration: img.src='/27.jpeg',
+        links: {
+         28:"28",
+         34:"34"
+        }
+    },
+    28=={
+        text: "page 28",
+        illustration: img.src='/28.jpeg',
+        links: {
+         34:"34",
+         39:"39"
+        }
+    },
+    29=={
+        text: "page 29",
+        illustration: img.src='/29.jpeg',
+        links: {
+         31:"31",
+         33:"33" 
+        }
+    },
+    30=={
+        text: "page 30",
+        illustration: img.src='/30.jpeg',
+        links: {
+         27:"27",
+         34:"34"
+        }
+    },
+    31=={
+        text: "page 31",
+        illustration: img.src='/31.jpeg',
+        links: {
+         34:"34",
+         40:"40"
+        }
+    },
+    32=={
+        text: "page 32",
+        illustration: img.src='/32.jpeg',
+        links: {
+            47:"47",
+            48:"48"
+        }
+    },
+    33=={
+        text: "page 33",
+        illustration: img.src='/33.jpeg',
+        links: {
+         47:"47",
+         48:"48"
+        }
+    },
+    34=={
+        text: "page 34",
+        illustration: img.src='/34.jpeg',
+        links: {
+         38:"38",
+         37:"37"  
+        }
+    },
+    35=={
+        text: "page 35",
+        illustration: img.src='/35.jpeg',
+        links: {
+         36:"36",
+         37:"37"  
+        }
+    },
+    36=={
+        text: "page 36",
+        illustration: img.src='/36.jpeg',
+        links: {
+            38:"38",
+            39:"39"
+        }
+    },
+    37=={
+        text: "page 37",
+        illustration: img.src='/37.jpeg',
+        links: {
+          39:"39",
+          45:"45"
+        }
+    },
+    38=={
+        text: "page 38",
+        illustration: img.src='/38.jpeg',
+        links: {
+         40:"40",
+         43:"43" 
+        }
+    },
+    39=={
+        text: "page 39",
+        illustration: img.src='/39.jpeg',
+        links: {
+         40:"40",
+         43:"43"
+        }
+    },
+    40=={
+        text: "page 40",
+        illustration: img.src='/40.jpeg',
+        links: {
+         41:"41",
+         42:"42" 
+        }
+    },
+    41=={
+        text: "page 41",
+        illustration: img.src='/41.jpeg',
+        links: {
+            19:"19",
+            44:"44"
+        }
+    },
+    42=={
+        text: "page 42",
+        illustration: img.src='/42.jpeg',
+        links: {
+         19:"19",
+         44:"44" 
+        }
+    },
+    43=={
+        text: "page 43",
+        illustration: img.src='/43.jpeg',
+        links: {
+         47:"47",
+         48:"48"  
+        }
+    },
+    44=={
+        text: "page 44",
+        illustration: img.src='/44.jpeg',
+        links: {
+            45:"45",
+            46:"46"
+        }
+    },
+    45=={
+        text: "page 45",
+        illustration: img.src='/45.jpeg',
+        links: {
+         47:"47",
+         48:"48" 
+        }
+    },
+    46=={
+        text: "page 46",
+        illustration: img.src='/46.jpeg',
+        links: {
+         7:"7",
+         65:"65"   
+        }
+    },
+    47=={
+        text: "page 47",
+        illustration: img.src='/47.jpeg',
+        links: {
+         49:"49",
+         50:"50"   
+        }
+    },
+    48=={
+        text: "page 48",
+        illustration: img.src='/48.jpeg',
+        links: {
+         51:"51",
+         55:"55"
+        }
+    },
+    49=={
+        text: "page 49",
+        illustration: img.src='/49.jpeg',
+        links: {
+          52:"52",
+          55:"55" 
+        }
+    },
+    50=={
+        text: "page 50",
+        illustration: img.src='/50.jpeg',
+        links: {
+         56:"56",
+         54:"54" 
+        }
+    },
+    51=={
+        text: "page 51",
+        illustration: img.src='/51.jpeg',
+        links: {
+         55:"55",
+         57:"57" 
+        }
+    },
+    52=={
+        text: "page 52",
+        illustration: img.src='/52.jpeg',
+        links: {
+         53:"53",
+         57:"57"
+        }
+    },
+    53=={
+        text: "page 53",
+        illustration: img.src='/53.jpeg',
+        links: {
+         55:"55",
+         57:"57"   
+        }
+    },
+    54=={
+        text: "page 54",
+        illustration: img.src='/54.jpeg',
+        links: {
+         55:"55",
+         57:"57"   
+        }
+    },
+    55=={
+        text: "page 55",
+        illustration: img.src='/55.jpeg',
+        links: {
+         56:"56",
+         66:"66"  
+        }
+    },
+    56=={
+        text: "page 56",
+        illustration: img.src='/56.jpeg',
+        links: {
+         60:"60",
+         61:"61" 
+        }
+    },
+    57=={
+        text: "page 57",
+        illustration: img.src='/57.jpeg',
+        links: {
+            58:"58",
+            59:"59"
+        }
+    },
+    58=={
+        text: "page 58",
+        illustration: img.src='/58.jpeg',
+        links: {
+         14:"14",
+         29:"29"
+        }
+    },
+    59=={
+        text: "page 59",
+        illustration: img.src='/59.jpeg',
+        links: {
+            61:"61",
+            66:"66"
+        }
+    },
+    60=={
+        text: "page 60",
+        illustration: img.src='/60.jpeg',
+        links: {
+         62:"62",
+         63:"63" 
+        }
+    },
+    61=={
+        text: "page 61",
+        illustration: img.src='/61.jpeg',
+        links: {
+         4:"4",
+         65:"65" 
+        }
+    },
+    62=={
+        text: "page 62",
+        illustration: img.src='/62.jpeg',
+        links: {
+         64:"64",
+         65:"65"
+        }
+    },
+    63=={
+        text: "page 63",
+        illustration: img.src='/63.jpeg',
+        links: {
+         64:"64",
+         70:"70"   
+        }
+    },
+    64=={
+        text: "page 64",
+        illustration: img.src='/64.jpeg',
+        links: {
+         4:"4",
+         65:"65"  
+        }
+    },
+    65=={
+        text: "page 65",
+        illustration: img.src='/65.jpeg',
+        links: {
+         1:"1"  
+        }
+    },
+    66=={
+        text: "page 66",
+        illustration: img.src='/66.jpeg',
+        links: {
+         67:"67",
+         68:"68"
+        }
+    },
+    67=={
+        text: "page 67",
+        illustration: img.src='/67.jpeg',
+        links: {
+         46:"46",
+         69:"69"  
+        }
+    },
+    68=={
+        text: "page 68",
+        illustration: img.src='/68.jpeg',
+        links: {
+         46:"46",
+         70:"70"  
+        }
+    },
+    69=={
+        text: "page 69",
+        illustration: img.src='/69.jpeg',
+        links: {
+         56:"56",
+         57:"57"   
+        }
+    },
+    70=={
+        text: "page 70",
+        illustration: img.src='/70.jpeg',
+        links: {
+         57:"57",
+         31:"31"  
+        }
+    },
+    71=={
+        text:"page 71",
+        illustration:img.src="",
+        links:{
+         47:"47",
+         9:"9"
+        }
+    },
+];
 
-
- //let link = //link needs to be not the text but the property of the links. the number before the txt string"//
- //let pageNum =//the same as link...i think somethin linke that//
- //let delayInMilliseconds=
  let pageDiv = document.getElementById("page");
- let disclaimer = document.getElementById("disclaimer-btn");
- let main = document.getElementById("main-btn");
- let credits = document.getElementById("credits-btn");
- let title = document.getElementById("title-btn");
+ let disclaimerBtnt = document.getElementById("disclaimer-btn-t");
+ let mainBtnd= document.getElementById("main-btn-d");
+ let mainBtnc= document.getElementById("main-btn-c");
+ let creditsBtnd= document.getElementById("credits-btn-d");
+ let creditsBtnm= document.getElementById("credits-btn-m");
+ let titleBtnd = document.getElementById("title-btn-d");
+ let titleBtnc = document.getElementById("title-btn-c");
+ let runningPage =0;
+ let curPage = pages[runningPage];
+ let jslinks= document.getElementById("js-links");
+ let runningIll=0;
+ let curIll = runningIll;
+
+
  
 
 function displayCredits() {
@@ -16,8 +593,9 @@ function displayCredits() {
     document.getElementById("main-body").style.display="none";
     document.getElementById("credits").style.display="block";
 
-    title.addEventListener("click",displayTitle);
-    main.addEventListener("click",displayMain);
+    titleBtnc.addEventListener("click", displayTitle);
+    mainBtnc.addEventListener("click", displayMain);
+
 };
 
 function displayMain() {
@@ -26,10 +604,14 @@ function displayMain() {
     document.getElementById("main-body").style.display="block";
     document.getElementById("credits").style.display="none";
 
-    credits.addEventListener("click", displayCredits);
-    
-
-};
+    jslinks.innerHTML="<p>" + curPage.text + "<p>";
+    curPage.links.forEach(links => {
+        var button = document.createElement ("button");
+        button.textContent=links
+        js-link.append(button)
+    });
+    creditsBtnm.addEventListener("click", displayCredits)
+    };
 
 function displayDisclaimer() {
     document.getElementById("title-page").style.display="none";
@@ -37,9 +619,9 @@ function displayDisclaimer() {
     document.getElementById("main-body").style.display="none";
     document.getElementById("credits").style.display="none";
 
-    main.addEventListener("click", displayMain);
-    title.addEventListener("click", displayTitle);
-    credits.addEventListener("click", displayCredits);
+    mainBtnd.addEventListener("click", displayMain);
+    titleBtnd.addEventListener("click", displayTitle);
+    creditsBtnd.addEventListener("click", displayCredits);
 
 };
 
@@ -49,7 +631,7 @@ function displayTitle() {
     document.getElementById("main-body").style.display="none";
     document.getElementById("credits").style.display="none";
     
-    disclaimer.addEventListener("click",displayDisclaimer);
+    disclaimerBtnt.addEventListener("click", displayDisclaimer);
 };
 
 displayTitle();
